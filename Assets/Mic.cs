1d8e38306cc561b0d6105fb18786c682fadc1c01
Luke Sanderson <cactuar18@hotmail.com>
2017-02-07 16:28:21 +0000

    Mic input works!
    progress is going too quick, some bugs are gonna pop up i just know it

--- Assets/Mic.cs
+++ Assets/Mic.cs
@@ -1,29 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Mic : MonoBehaviour {
+public class Mic : MonoBehaviour
+{
 
 
     AudioSource audio;
-	// Use this for initialization
-	void Start () {
+
+    public float sensitivity = 100f;
+    public float loud = 0;
+    // Use this for initialization
+    void Start()
+    {
+        foreach (string device in Microphone.devices)
+        {
+            Debug.Log("Name: " + device);
+        }
 
         audio = GetComponent<AudioSource>();
         audio.loop = true;
         audio.mute = true;
         //record from first audio device, loop it, 10 seconds length, Hz;
-        audio.clip = Microphone.Start(null, true, 10, 44100);
+        audio.clip = Microphone.Start(null, true, 2, 44100);
 
-        while (!(Microphone.GetPosition(AudioInputDevice) > 0)) { } // Wait until the recording has started
+        while (!(Microphone.GetPosition(Microphone.devices[0]) > 0)) { } // Wait until the recording has started
+        audio.mute = false;
         audio.Play();
 
-   
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+
+    }
+
+    public float AverageVol()
+    {
+        float[] data = new float[256]; //lovely number
+        audio.GetOutputData(data, 0); //last int is channel
+        float i = 0;                       //HAS TO BE ZERO GRR
+        foreach (float asda in data)
+        {
+            i += Mathf.Abs(asda);
+        }
+        return i / data.Length; //get the mean
+    }
+
+    void Update()
+    {
+        loud = AverageVol() * sensitivity;
+        transform.GetChild(0).GetChild(0).GetComponent<Text>().text = "" + loud;
+    }
 }
---
+++ Assets/SpawnTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTest : MonoBehaviour {
+
+    public GameObject micManager;
+    public Mic mic;
+
+    public float threshold = 1f;
+    public GameObject prefab;
+	// Use this for initialization
+	void Start () {
+        micManager = GameObject.Find("Mic Manager");
+        mic = micManager.GetComponent<Mic>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float i = mic.loud;
+        if (i > threshold)
+        {
+            print("WORKS WORKS WORKS!");
+            Vector3 test = Vector3.one;
+            GameObject an = Instantiate(prefab, transform.position, Quaternion.identity);
+            an.transform.localScale += test;
+        }
+	}
+}
