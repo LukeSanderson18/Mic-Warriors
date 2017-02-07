d875120c52d90518e01d9161f87f0d2737112ce6
Luke Sanderson <cactuar18@hotmail.com>
2017-02-07 14:51:50 +0000

    Fixed git-ness
    cos git is rubbish

---
+++ Assets/Mic.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mic : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        print("test for git");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
