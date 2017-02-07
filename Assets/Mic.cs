a69f9ab49651e68676561904aac6e279416c1aaf
Luke Sanderson <cactuar18@hotmail.com>
2017-02-07 15:18:23 +0000

    bugs make me cry

--- Assets/Mic.cs
+++ Assets/Mic.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class Mic : MonoBehaviour {
 
+
+    AudioSource audio;
 	// Use this for initialization
 	void Start () {
 
-        print("test for git");
+        audio = GetComponent<AudioSource>();
+        audio.loop = true;
+        audio.mute = true;
+        //record from first audio device, loop it, 10 seconds length, Hz;
+        audio.clip = Microphone.Start(null, true, 10, 44100);
+
+        while (!(Microphone.GetPosition(AudioInputDevice) > 0)) { } // Wait until the recording has started
+        audio.Play();
+
+   
 		
 	}
 	
